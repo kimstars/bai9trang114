89e1c72e0a7911dc391427171513165c35d7b05e
Khong <shinkhong97@gmail.com>
2021-02-27 04:00:06 -0800

    Add new class pullRe

---
+++ company/pullRequest.java
@@ -0,0 +1,9 @@
+// Just an Example
+
+// Pull requested 
+// 1. checkout -b <feature/branch>
+// 2. git push oorigin <branch>
+// 3. create a pull request on GitHub
+// 4. review code 
+// 5. merge to master 
+// from Thao xinh dep 
